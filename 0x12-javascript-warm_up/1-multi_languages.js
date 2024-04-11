b18ae9c422052a4a9d06dc43a410be35234a591d
CLEMENT-OLATOYE <olatoyeclement@gmail.com>
2024-04-11 05:10:35 -0700

    done

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is school');
-console.log('JavaScript is amazing');
+console.log("C is fun");
+console.log("Python is school");
+console.log("JavaScript is amazing");
