a9aff9c7256942a1f5a143ac70598937bac842c3
CLEMENT-OLATOYE <olatoyeclement@gmail.com>
2024-04-29 01:17:31 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,10 +8,10 @@
 
 if __name__ == "__main__":
         import sys
-        from urllib import request, error
+        from urllib import requests, error
 
         try:
-            with request.urlopen(sys.argv[1]) as res:
+            with requests.urlopen(sys.argv[1]) as res:
                 print(res.read().decode('UTF-8'))
         except error.HTTPError as er:
             print('Error code:', er.code)
